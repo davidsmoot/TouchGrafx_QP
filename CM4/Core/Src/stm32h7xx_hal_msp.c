88e5dac2594a82ae731ef880b017070f318126d8
David Smoot <dsmoot@sparxeng.com>
2021-11-09 07:51:57 -0600

    Enable UART8 interrupt, change interrupt priorities.

--- CM4/Core/Src/stm32h7xx_hal_msp.c
+++ CM4/Core/Src/stm32h7xx_hal_msp.c
@@ -115,6 +115,9 @@ void HAL_UART_MspInit(UART_HandleTypeDef* huart)
     GPIO_InitStruct.Alternate = GPIO_AF8_UART8;
     HAL_GPIO_Init(GPIOJ, &GPIO_InitStruct);
 
+    /* UART8 interrupt Init */
+    HAL_NVIC_SetPriority(UART8_IRQn, 0, 0);
+    HAL_NVIC_EnableIRQ(UART8_IRQn);
   /* USER CODE BEGIN UART8_MspInit 1 */
 
   /* USER CODE END UART8_MspInit 1 */
@@ -144,6 +147,8 @@ void HAL_UART_MspDeInit(UART_HandleTypeDef* huart)
     */
     HAL_GPIO_DeInit(GPIOJ, GPIO_PIN_9|GPIO_PIN_8);
 
+    /* UART8 interrupt DeInit */
+    HAL_NVIC_DisableIRQ(UART8_IRQn);
   /* USER CODE BEGIN UART8_MspDeInit 1 */
 
   /* USER CODE END UART8_MspDeInit 1 */
